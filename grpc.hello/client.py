39d18fbe1e46767cc9985c8637edb19029cb8ed2
Ana Rubio Ruiz <Ana.Rubio@uclm.es>
2025-10-13 13:23:46 +0200

    grpc.hello: update client

--- grpc.hello/client.py
+++ grpc.hello/client.py
@@ -15,7 +15,7 @@ server = sys.argv[1]
 port = sys.argv[2]
 
 channel = grpc.insecure_channel(f'{server}:{port}')
-stub = hello_pb2_grpc.HelloStub(channel)
 
+stub = hello_pb2_grpc.HelloStub(channel)
 message = hello_pb2.PrintRequest(message='hello')
 stub.write(message)
