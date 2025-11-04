c30377d3ab272d4000d1a1eac009217cb0f10f1a
Ana Rubio Ruiz <Ana.Rubio@uclm.es>
2025-11-04 11:06:49 +0100

    rabbitmq.pubsub-pattern: ready

--- rabbitmq.publish-subscribe-pattern/subscriber.py
+++ rabbitmq.publish-subscribe-pattern/subscriber.py
@@ -1,13 +1,19 @@
 #!/usr/bin/python3
 # Extracted from: https://www.rabbitmq.com/tutorials/tutorial-five-python.html
+'Usage: {0} [binding key]...\n'
 
 import sys
 import pika
 
 
 def callback(ch, method, properties, body):
-    print("[x] Received %r: %r " % (method.routing_key, body.decode("UTF-8")))
+    print(f"[x] Received {method.routing_key}: {body.decode()}")
 
+
+binding_keys = sys.argv[1:]
+if not binding_keys:
+    sys.stderr.write(__doc__.format(sys.argv[0]))
+    sys.exit(1)
 
 localhost = pika.ConnectionParameters(host='localhost')
 connection = pika.BlockingConnection(localhost)
@@ -17,23 +23,23 @@ channel.exchange_declare(exchange='twitter-pattern', exchange_type='topic')
 result = channel.queue_declare(queue='', exclusive='True')
 queue_name = result.method.queue
 
-binding_keys = sys.argv[1:]
-if not binding_keys:
-    sys.stderr.write("Usage: {} [binding key]...\n".format(sys.argv[0]))
-    sys.exit(1)
-
 for binding_key in binding_keys:
     channel.queue_bind(
-        exchange='twitter-pattern',
         queue=queue_name,
-        routing_key=binding_key
+        routing_key=binding_key,
+        exchange='twitter-pattern',
     )
 
 channel.basic_consume(
-    on_message_callback=callback,
     queue=queue_name,
-    auto_ack=True
+    on_message_callback=callback,
+    auto_ack=True,
 )
 
 print("[*] Waiting for messages. To exit press Ctrl+")
-channel.start_consuming()
+
+try:
+    channel.start_consuming()
+except KeyboardInterrupt:
+    print("\nStopping subscriber...")
+    connection.close()
